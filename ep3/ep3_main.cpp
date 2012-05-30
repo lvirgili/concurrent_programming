9a888b278e180e0b88a82549c1bee61974b6ea3a
Lucas Virgili <luvirgili@gmail.com>
2012-05-30 16:35:20 -0300

    argc

--- ep3/ep3_main.cpp
+++ ep3/ep3_main.cpp
@@ -5,7 +5,20 @@
 #include <ctime>
 using namespace std;
 
-int main() {
+int main(int argc, char **argv) {
+     int C, m; //C = capacidade do carro, m = no de carros
+     double t; //t = taxa
+     if (argc != 4) {
+          cout << "Uso: ./ep3 C m t\n";
+          return 0;
+     } else {
+          C = atoi(argv[1]);
+          m = atoi(argv[2]);
+          t = strtod(argv[3], NULL);
+     }
+     cout << "C = " << C << endl;
+     cout << "m = " << m << endl;
+     cout << "t = " << t << endl;
      srand(time(NULL));
      priority_queue<Passageiro, vector<Passageiro>, comp_passageiro> fila;
      for (int i = 0; i < 5; ++i) {
